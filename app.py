6841afd37ba0205f4b4a68b1e3d5210b75a1584f
mark-solo <mark.an.impostor@gmail.com>
2018-12-26 11:11:35 +0300

    secont test

---
+++ app.py
@@ -0,0 +1,15 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+	abort, render_template, flash, jsonify
+	
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'my_precious'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+	
+if __name__ == '__main__':
+	app.run()
