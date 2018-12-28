b5a0317359da00825753f0e1a63a9146eb26c466
mark-solo <mark.an.impostor@gmail.com>
2018-12-28 11:23:31 +0300

    javascript

--- app.py
+++ app.py
@@ -75,6 +75,19 @@ def add_entry():
 	flash('New entry was successfully posted')
 	return redirect(url_for('index'))
 	
+@app.route('/delete/<post_id>', methods=['GET'])
+def delete_entry(post_id):
+	result = {'status': 0, 'message': 'Error'}
+	try:
+		db = get_db()
+		db.execute('DELETE FROM entries WHERE id='+post_id)
+		db.commit()
+		result = {'status': 1, 'message': 'Post deleted'}
+	except Exception as e:
+		result = {'status': 0, 'message': repr(e)}
+	
+	return jsonify(result)
+	
 if __name__ == '__main__':
 	init_db()
 	app.run()
